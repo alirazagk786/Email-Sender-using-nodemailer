b84aa35806874dc28d8977c0f9b92aa67971b74a
Ftanwir1 <fahad.tanwir@gsgus.com>
2022-07-19 11:12:40 -0400

    Initial commmit

---
+++ Cruds.js
@@ -0,0 +1,7 @@
+module.exports={
+    senderMail : "xyz@gmail.com", //add sender mail here.
+    receiverMail : "abc@gmail.com", //add receiver mail here.
+    //please use app password here not your actual google account password.
+    //If you don't know how to generate app password then please check on google first.
+    password : "***************"
+}
